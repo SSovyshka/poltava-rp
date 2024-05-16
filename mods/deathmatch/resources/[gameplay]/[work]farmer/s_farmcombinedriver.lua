89098b22c584953dd23399becd05e6d0ea0471db
Богдан Савченко <aze.bog@gmail.com>
2024-05-17 02:05:12 +0300

    ,

---
+++ mods/deathmatch/resources/[gameplay]/[work]farmer/s_farmcombinedriver.lua
@@ -0,0 +1,27 @@
+-- local getCombineBlip = createMarker(-1091.85229, -1204.74951, 128.21875, "cylinder", 1.25, 255, 255, 255, 150)
+
+
+-- local combineCollider123123 = createColCuboid( -1133.51196, -1168.76794, 128.21875, 5, 5, 5 )
+
+-- addEventHandler("onResourceStart", resourceRoot, function()
+--     local combine = createVehicle(532, -1104.48303, -1182.97290, 129.2187)
+
+--     local combineCollider = createObject(3374, 0, 0, 0)
+--     setObjectScale(combineCollider, 2.25, 0.5, 0.5)
+
+--     attachElements(combineCollider, combine, 0, 5, -1)
+--     setElementCollisionsEnabled(combineCollider, false)
+
+--     addEventHandler ( "onVehicleEnter", resourceRoot, function() 
+--         setElementCollisionsEnabled(combineCollider, true)
+--     end)
+
+--     addEventHandler ( "onVehicleExit", resourceRoot, function() 
+--         setElementCollisionsEnabled(combineCollider, false)
+--     end)
+
+-- end)
+
+
+
+
